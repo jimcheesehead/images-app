b5fb98f9b12a441bf38ba23755e1706dac806bf5
Jim Gregor <jimmyg0801@gmail.com>
2020-03-27 16:18:51 -0400

    Finished: 126. Listing Images Finished: Section 7: Design of Vuex Modules

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>Image List</div>
+  <div>
+    <img v-for="image in allImages" :key="image.id" :src="image.link" width="200" />
+  </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'ImageList',
+  computed: mapGetters(['allImages']),
   methods: mapActions(['fetchImages']),
   created() {
     this.fetchImages();
